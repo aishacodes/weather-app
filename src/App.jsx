a8534f02a5c4c8052206f1541416dd08b6d4f567
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-04-15 22:40:32 +0100

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,113 @@
 import { useState, useEffect } from "react";
 import Axios from "axios";
 import "./App.css";
+// import { Line } from "react-chartjs-2";
 
 function App() {
-  const [city, setCity] = useState("Ilorin");
+  const [city, setCity] = useState("Kenya");
   const [cityData, setCityData] = useState([]);
   const [activeIndex, setActiveIndex] = useState(0);
+  const [curW, setCurw] = useState([]);
   const apiKey = process.env.REACT_APP_API_KEY;
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      const successfulLookup = (position) => {
-        const { latitude, longitude } = position.coords;
-        Axios.get(
-          `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`
-        ).then((res) => {
-          // console.log(res.data.list);
-          const reading = res.data.list.filter((dt) =>
-            dt.dt_txt.includes("18:00:00")
-          );
-          console.log(reading);
-          setCityData(reading);
-        });
-      };
-      window.navigator.geolocation.getCurrentPosition(
-        successfulLookup,
-        console.log
-      );
-    } else {
+    const successfulLookup = (position) => {
+      const { latitude, longitude } = position.coords;
       Axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`
       ).then((res) => {
-        // console.log(res.data.list);
+        console.log(res.data.city);
         const reading = res.data.list.filter((dt) =>
           dt.dt_txt.includes("18:00:00")
         );
         console.log(reading);
         setCityData(reading);
       });
-    }
+      //current weather
+      Axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      ).then((res) => {
+        console.log(res.data);
+        setCurw("hhhh", res.data);
+      });
+    };
+
+    const unSuccessful = () => {
+      Axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=London&units=imperial&appid=${apiKey}`
+      ).then((res) => {
+        console.log(res.data.city);
+        const reading = res.data.list.filter((dt) =>
+          dt.dt_txt.includes("18:00:00")
+        );
+        console.log(reading);
+        setCityData(reading);
+      });
+      //current weather
+      Axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      ).then((res) => {
+        console.log(res.data);
+        setCurw(res.data);
+      });
+    };
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 10000,
+      maximumAge: 0,
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      successfulLookup,
+      unSuccessful,
+      options
+    );
   }, []);
+
+  useEffect(() => {
+    Axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+    ).then((res) => {
+      console.log(res.data.city);
+      const reading = res.data.list.filter((dt) =>
+        dt.dt_txt.includes("18:00:00")
+      );
+      console.log(reading);
+      setCityData(reading);
+    });
+  }, [city]);
+
+  const tempGraph = {
+    labels: ["January", "February", "March", "April", "May"],
+    datasets: [
+      {
+        label: "Rainfall",
+        fill: true,
+        lineTension: 0.5,
+        backgroundColor: "rgba(75,192,192,1)",
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: cityData.map((t) => t.main.temp),
+      },
+    ],
+  };
 
   return (
     <div className="App grid grid-cols-3 ">
       <div className="w-full py-4 px-6">
         <p className="flex items-center">
           <span className="text-2xl font-extrabold">Your City</span>
-          <span className="pr-9 pl-2 py-1 border border-gray-300 mx-6">
+          <input
+            type="text"
+            name=""
+            placeholder={city}
+            className="pr-6 pl-2 py-1 border border-gray-300 mx-6"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          {/* <span className="pr-9 pl-2 py-1 border border-gray-300 mx-6">
             {city}
-          </span>
+          </span> */}
         </p>
 
         {cityData.length > 0 ? (
@@ -62,7 +121,7 @@ function App() {
             </p>
             <div className="flex items-center">
               <img
-                src={`http://openweathermap.org/img/w/${cityData[activeIndex].weather[0].icon}.png
+                src={`http://openweathermap.org/img/wn/${cityData[activeIndex].weather[0].icon}@2x.png
 `}
                 className="w-32 h-32"
                 alt=""
@@ -95,11 +154,37 @@ function App() {
           "Loading...."
         )}
       </div>
-      <div className="col-span-2 grid grid-rows-3 ">
-        <div className="row-span-2">
+      <div className="col-span-2 grid grid-rows-2 ">
+        <div className="">
           <p>Temperature</p>
+          {/* <Line
+            data={tempGraph}
+            options={{
+              title: {
+                display: true,
+                text: "Temperature",
+                fontSize: 20,
+              },
+              legend: {
+                display: true,
+                position: "right",
+              },
+            }}
+          />{" "} */}
         </div>
         <div className="flex">
+          <div className="p-6 mx-4 flex flex-col justify-center text-center items-center">
+            <p>{new Date(curW.dt * 1000).toDateString("en-US")}</p>
+            <img
+              src={`http://openweathermap.org/img/wn/${curW.weather[0].icon}@2x.png
+`}
+              alt=""
+            />
+            <p>
+              Humidity <br /> {curW.main.humidity}%
+            </p>
+          </div>
+
           {cityData.map((cityd, citydIndex) => (
             <div
               className={`p-6 mx-4 flex flex-col justify-center text-center items-center ${
@@ -109,7 +194,7 @@ function App() {
             >
               <p>{new Date(cityd.dt * 1000).toDateString("en-US")}</p>
               <img
-                src={`http://openweathermap.org/img/w/${cityd.weather[0].icon}.png
+                src={`http://openweathermap.org/img/wn/${cityd.weather[0].icon}@2x.png
 `}
                 alt=""
               />
